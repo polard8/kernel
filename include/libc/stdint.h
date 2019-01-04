6d4bd7f03a321cfd96e08a19f1f891748ced9cc1
Fred Nora <freddynora@gmail.com>
2019-01-04 21:45:49 -0200

    makefiles

--- include/libc/stdint.h
+++ include/libc/stdint.h
@@ -1,6 +1,21 @@
 
 
+
+#ifndef __STDINT_H__
+#define __STDINT_H__
+
 //stdint.h
 
 // Definição de tipos de dados inteiros.
+
+//?? #bugbgu
+//typedef long long intmax_t;
+//typedef unsigned long long uintmax_t;
+
+typedef long intmax_t;
+typedef unsigned long uintmax_t;
+
+
+#endif 
+
 
