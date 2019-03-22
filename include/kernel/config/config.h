bda1e6ad1234631d19375789d24cf675a005d400
frednora <freddynora@gmail.com>
2019-03-22 19:02:49 -0300

    rc7

--- include/kernel/config/config.h
+++ include/kernel/config/config.h
@@ -11,6 +11,8 @@
 // ## IMPORNTANTE ##
 //
 
+//TEM QUE CONFIGURAR O BL TAMBÉM
+
 // usaremos essa configuraçao provisoriamente
 // ate que tenhamos a condiçao de selecionarmos corretamente
 // o canal e o dispositivo.
@@ -21,16 +23,29 @@
 // USE PRIMARY MASTER!!
 
 // Portas bases encontradas nas BARs.
-//BAR0 = base port 1F0   HD primário
-//BAR1 = base port 3F6	
-//BAR2 = base port 170   HD secundário
-//BAR3 = base port 376
-	
-//#importante: Isso é BAR e naõ porta.
-#define  __IDE_PORT __IDE_BAR
-#define __IDE_BAR 0
+//BAR0 = base port 1F0   ( channel 0 primary master) (channel 0 primary slave) 
+//BAR1 = base port 3F6	 channel ??
+//BAR2 = base port 170   (channel 1 secondary master) (channel 1 secondary slave)
+//BAR3 = base port 376   channel ??
 
-#define __IDE_SLAVE  0  // 0=master 1=slave
+
+// #importante
+// master e slave é coisa do PATA
+// então 3f6 pode ser canal 2 e 376 pode ser canal 3.
+	
+
+#define __CHANNEL0 0  // BAR 0 primary, master and slave
+#define __CHANNEL1 2  // BAR 2 secondary, master and slave
+#define __CHANNEL2 1  // BAR 1 extra (sata amulating ide)
+#define __CHANNEL3 3  // BAR 3 extra (sata amulating ide)
+
+
+// #importante
+// Esses valores são usados pelo driver.
+#define __IDE_PORT __CHANNEL0    //primary
+#define __IDE_SLAVE  0           // 0=master 1=slave
+
+
 
 
 // #tests:
@@ -54,6 +69,40 @@
 // Na verdade só funcionam as portas 0 e 2 porque são 
 // selecionadores das BARs 0 e 2, onde estão as portas de HD.
 // #todo: rever o código nessa parte de configuração das BARs.
+
+
+//a questao 'e que existem canais extras
+//vamos presizar ler mais bars.
+
+/*
+Current disk controller chips almost always support two ATA buses per chip. 
+There is a standardized set of IO ports to control the disks on the buses. 
+The first two buses are called the Primary and Secondary ATA bus, and are almost 
+always controlled by IO ports 0x1F0 through 0x1F7, and 0x170 through 0x177, 
+respectively (unless you change it). The associated 
+Device Control Registers/Alternate Status ports are IO ports 0x3F6, and 0x376,
+respectively. The standard IRQ for the Primary bus is IRQ14, and IRQ15 for the Secondary bus.
+
+If the next two buses exist, they are normally controlled by IO ports 0x1E8 through 0x1EF, 
+and 0x168 through 0x16F, respectively. The associated
+Device Control Registers/Alternate Status ports are IO ports 0x3E6, and 0x366. 
+*/
+
+/*
+This is the multiple IDE interface driver, as evolved from hd.c.
+
+It supports up to 9 IDE interfaces per default, on one or more IRQs (usually
+14 & 15).  There can be up to two drives per interface, as per the ATA-6 spec.
+
+Primary:    ide0, port 0x1f0; major=3;  hda is minor=0; hdb is minor=64
+Secondary:  ide1, port 0x170; major=22; hdc is minor=0; hdd is minor=64
+Tertiary:   ide2, port 0x1e8; major=33; hde is minor=0; hdf is minor=64
+Quaternary: ide3, port 0x168; major=34; hdg is minor=0; hdh is minor=64
+fifth..     ide4, usually PCI, probed
+sixth..     ide5, usually PCI, probed
+*/
+
+
 
 
 #define FRED_VHD_32MB
