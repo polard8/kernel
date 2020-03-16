13d40388195759ec051d5ca550dcb2e940aa21ee
frednora <freddynora@gmail.com>
2020-03-16 17:35:03 -0300

    ~

--- scripts/git/c1.sh
+++ scripts/git/c1.sh
@@ -1,4 +1,4 @@
-git filter-branch -f --index-filter 'git rm --cached --ignore-unmatch *.BIN' --tag-name-filter cat -- --all    
+git filter-branch -f --index-filter 'git rm --cached --ignore-unmatch sdebug.txt' --tag-name-filter cat -- --all    
 
 
 #git push origin --force --all    
