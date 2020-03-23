97da0e471bc5b3657ffe933152734b69502cc52b
frednora <freddynora@gmail.com>
2020-03-23 15:00:22 -0300

    Create lib/gdeio01

--- lib/gdeio01/include/gdeio.h
+++ lib/gdeio01/include/gdeio.h
@@ -1,10 +1,14 @@
+/*
+ * File: gdeio.h
+ * 
+ * 2019 - Created by Fred Nora.
+ */
 
 
 //retorna o valor.
 unsigned char gde_inport8 (unsigned short port);
 unsigned short gde_inport16 (unsigned short port);
 unsigned long gde_inport32 (unsigned short port);
-	
 
 void gde_outport8 ( unsigned short port, unsigned char value);
 void gde_outport16 ( unsigned short port, unsigned short value);
