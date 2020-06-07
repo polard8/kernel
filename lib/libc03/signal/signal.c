f9e0750d01f35701025c3aaaa14dbd6cc57c7c7c
frednora <freddynora@gmail.com>
2020-06-07 17:52:35 -0300

    libc03: signal

--- lib/libc03/signal/signal.c
+++ lib/libc03/signal/signal.c
@@ -61,24 +61,23 @@ const char *sys_siglist[NSIG] = {
     
 sighandler_t signal (int signum, sighandler_t handler){
 
-    sighandler_t __ret;
-
-
-    debug_print ("signal: [TODO] \n");
-
-    /*
-    ret = (sighandler_t) gramado_system_call ( ?, ?, ?, ? );
-
-    if (ret == SIG_ERR)
-	{
-		errno = EINVAL;
-		return (SIG_ERR);
-	}
-	
-	return (ret);
-	*/
-
-    return handler;  //#todo
+    struct sigaction new_act;
+    struct sigaction old_act;
+    
+    
+    debug_print ("signal: [TODO] \n");    
+    
+    new_act.sa_handler = handler;
+    new_act.sa_flags = 0;
+    new_act.sa_mask = 0;
+    
+    int rc = sigaction(signum, &new_act, &old_act);
+    
+    if (rc < 0)
+        return SIG_ERR;
+    
+    // ?? #bugbug: Qual é esse valor ??
+    return old_act.sa_handler;
 }
 
 
