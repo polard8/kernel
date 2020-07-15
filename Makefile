446e0893f2986de5041ee7777d888dcd9487960b
frednora <freddynora@gmail.com>
2020-07-14 21:06:57 -0300

    mk

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 # License: BSD License
 VERSION = 1
 PATCHLEVEL = 0
-SUBLEVEL = 47
+SUBLEVEL = 48
 EXTRAVERSION = 
 NAME = Gramado 1.0
 
--- shine/cmd/uname/main.c
+++ shine/cmd/uname/main.c
@@ -3,11 +3,26 @@
 #include <sys/utsname.h>
 
 
+/*
+//crazy test
+void print_4_chars( unsigned long value )
+{
+    char chars[4];
+    
+    chars[0] = (char) value;
+    chars[1] = (char) (value >> 8);
+    chars[2] = (char) (value >> 16);
+    chars[3] = (char) (value >> 24);
+
+    printf ("%c%c%c%c",
+        chars[0],chars[1],chars[2],chars[3]); 
+    fflush(stdout);
+}
+*/
 
 int main (int argc, char **argv){
 
     struct utsname name;
-
 
 
     uname (&name);
@@ -16,8 +31,16 @@ int main (int argc, char **argv){
         name.sysname, name.release, name.version,
         name.machine, name.nodename );
 
+
+    //crazy test  efgh
+    //while(1){
+    //    print_4_chars(0xFFFFFFFF);
+    //}
+    
     return 0;
 }
+
+
 
 
 
