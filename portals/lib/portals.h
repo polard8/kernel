aa1c307475ce727f71745f53c2f1fec293b0d5a5
nora <freddynora@gmail.com>
2020-08-30 11:21:06 -0300

    lib: Create portals.h

---
+++ portals/lib/portals.h
@@ -0,0 +1,20 @@
+// This is a wrapper for libgws and some other possible libs.
+// More: Entry point for applications and system information.
+
+
+// Callint the window server. 
+#include "libgws/gws.h"
+
+
+//
+// Application entry point prototype
+//
+
+int portals_main (int argc, char *argv[]);
+
+//
+//  System information
+//
+
+// ...
+
