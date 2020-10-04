ced3c6b27b75fdd379de15dc27e74f67610a53bd
nora <freddynora@gmail.com>
2020-10-04 09:29:39 -0300

    gdeshell: net support

--- gramnt/setup/gdeshell/net.h
+++ gramnt/setup/gdeshell/net.h
@@ -1,32 +1,39 @@
 
+// net.h
+// 
+
+
 #ifndef __NET_H
 #define __NET_H    1
 
 
 
-#define ETH_TYPE_IP   0x0800  
-#define ETH_TYPE_ARP  0x0806
+#define ETH_TYPE_IP     0x0800  
+#define ETH_TYPE_ARP    0x0806
+ 
  
 #define ARP_OPC_REQUEST  0x01
 #define ARP_OPC_REPLY    0x02
 
 
-//little endian
-#define ToNetByteOrder16(v) ((v >> 8) | (v << 8))
-#define ToNetByteOrder32(v) (((v >> 24) & 0xFF) | ((v << 8) & 0xFF0000) | ((v >> 8) & 0xFF00) | ((v << 24) & 0xFF000000))
+
+// little endian
+#define ToNetByteOrder16(v)   ((v >> 8) | (v << 8))
+#define ToNetByteOrder32(v)   (((v >> 24) & 0xFF) | ((v << 8) & 0xFF0000) | ((v >> 8) & 0xFF00) | ((v << 24) & 0xFF000000))
 #define FromNetByteOrder16(v) ((v >> 8) | (v << 8))
 #define FromNetByteOrder32(v) (((v >> 24) & 0xFF) | ((v << 8) & 0xFF0000) | ((v >> 8) & 0xFF00) | ((v << 24) & 0xFF000000))
-
 
 //extra
 #define gdeshell_FromNetByteOrder16(v) ((v >> 8) | (v << 8))
 
 
 
+//
+// == Ethernet ==============================================
+//
+
 // Ethernet header length
-#define ETHERNET_HEADER_LENGHT 14      
-
-
+#define ETHERNET_HEADER_LENGHT  14  
 
 // ethernet header
 struct gdeshell_ether_header 
@@ -38,10 +45,13 @@ struct gdeshell_ether_header
 } __attribute__((packed)); 
 
 
+//
+// == ARP =================================================
+//
+
 
 // ARP header length
 #define ARP_HEADER_LENGHT 28      
-
 
 // arp struct
 struct  gdeshell_ether_arp {
@@ -65,12 +75,47 @@ struct  gdeshell_ether_arp {
 
 
 
-void 
-__SendARP ( uint8_t source_ip[4], 
-          uint8_t target_ip[4], 
-          uint8_t target_mac[6] );
+//
+// == Prototypes ==============================================
+//
 
+// Send ARP.
+void 
+__SendARP ( 
+    uint8_t source_ip[4], 
+    uint8_t target_ip[4], 
+    uint8_t target_mac[6] );
+
+
+// Test ARP.
 void __shellTestARP (void);
+
+
+// Initialize the network low level support.
+void network_initialize (void);
+
+// Send packet.
+// Direto. Envia o endereço do pacote para o kernel.
+// So lembrando que o lernel nao fara procesamentos
+// de protocolos de rede.
+
+void gdeshell_send_packet( unsigned long packet_buffer );
+
+void 
+print_ethernet_header ( 
+    const unsigned char *Buffer, 
+    int Size );
+
+int 
+gdeshell_decode_buffer ( unsigned long buffer_address );
+
+
+void network_test_buffer(void);
+
+
+// Testa rotinas do kernel.
+// Nao eh socket unix.
+void shellSocketTest (void);
 
 
 #endif    
