d93a7db1b128a9f611bf0bea148b0daccabebf18
nora <freddynora@gmail.com>
2021-02-12 18:03:44 -0300

    gwssrv: sprites

--- gramado/core/aurora/server/view/sprite.c
+++ gramado/core/aurora/server/view/sprite.c
@@ -4,13 +4,6 @@
 #include <gws.h>
 
 
-// #prototype.
-int 
-PlotSprite3D ( 
-    struct gws_display_d *display,
-    int x, int y, int z,
-    struct gws_sprite_d *sprite_list,
-    int list_size );
 
 
 /*
@@ -26,17 +19,104 @@ int
 PlotSprite3D ( 
     struct gws_display_d *display,
     int x, int y, int z,
-    struct gws_sprite_d *sprite_list,
-    int list_size )
+    unsigned long sprite_list_address,
+    int sprite_list_size,
+    int just_one )
 {
-    
+
+    unsigned long *list = (unsigned long *) sprite_list_address;
+
+    struct gws_sprite_d *sprite;
+    int Type = SPRITE_NULL;
+
+    int i=0;
+    int number_of_elements = sprite_list_size;
+    int Max = SPRITE_MAX_NUMBER;
+
+
     gwssrv_debug_print("PlotSprite3D: [TODO] \n");
-    return -1;
+ 
+    // invalid address
+    if ( sprite_list_address == 0 )
+    {
+        gwssrv_debug_print("PlotSprite3D: [FAIL] sprite_list_address\n");
+        return -1;
+    }
+
+    // no elements
+    if ( number_of_elements <= 0 )
+    {
+        gwssrv_debug_print("PlotSprite3D: [FAIL] no elements\n");
+        return -1;
+    }
+
+    // too much elements
+    if ( number_of_elements >= SPRITE_MAX_NUMBER )
+    {
+        gwssrv_debug_print("PlotSprite3D: [FAIL] number_of_elements\n");
+        return -1;
+    }
+
+
+    // Loop: 
+    // todos os elementos da lista.
+    
+    for(i=0; i<number_of_elements; i++){
+
+
+    // Get sprite structure.
+
+    sprite = (struct gws_sprite_d *) list[i];
+
+    // last one.
+    if ( (void*) sprite == NULL ){
+        gwssrv_debug_print("PlotSprite3D: [ERROR] sprite \n");
+        break;
+    }
+
+    if ( sprite->used != 1 || sprite->magic != 1234 ){
+        gwssrv_debug_print("PlotSprite3D: [ERROR] sprite validation\n");
+        break;
+    }
+
+    // type
+    Type = sprite->type;
+    switch (Type){
+
+        // #todo
+        // Insert here the primitives.
+        
+        case SPRITE_NULL:
+            gwssrv_debug_print("PlotSprite3D: [SPRITE_NULL] \n");
+            break;
+        
+        case SPRITE_LINE:
+            gwssrv_debug_print("PlotSprite3D: [SPRITE_LINE] \n");
+            break;
+
+        case SPRITE_CIRCLE:
+            gwssrv_debug_print("PlotSprite3D: [SPRITE_CIRCLE] \n");
+            break;
+            
+        // ...
+        
+        default:
+            gwssrv_debug_print("PlotSprite3D: [default] \n");
+            break;
+
+    };
+    
+        if ( just_one == TRUE )
+            break;
+    };
+   
+
+    // #todo
+    // Restaura para o padrao os elemento da estrutura display.
+    // device context
+    
+    gwssrv_debug_print("PlotSprite3D: done \n");
+    
+    return 0;
 }
-
-
-
-
-
-
 
--- gramado/core/aurora/server/view/sprite.h
+++ gramado/core/aurora/server/view/sprite.h
@@ -1,11 +1,53 @@
 
 
+#ifndef  ____SPRITE_H
+#define  ____SPRITE_H    1
+
+
+// Types
+// #todo: maybe create a 'enum'
+
+#define SPRITE_NULL   0
+#define SPRITE_LINE   1
+#define SPRITE_CIRCLE 2
+// ...
+
 
 struct gws_sprite_d
 {
     int used;
     int magic;
     
+    int type;
+    
     // ...
 };
+
+#define SPRITE_MAX_NUMBER 32
+// Sprite list.
+unsigned long spriteList[SPRITE_MAX_NUMBER];
+
+
+
+
+//
+// == prototypes ==============
+//
+
+
+int 
+PlotSprite3D ( 
+    struct gws_display_d *display,
+    int x, int y, int z,
+    unsigned long sprite_list_address,
+    int sprite_list_size,
+    int just_one );
+
+
+
+
+#endif    
+
+
+
 
