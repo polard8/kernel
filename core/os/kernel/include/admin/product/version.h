a865b4bbc9914a707fdbbf6f1f8381f7a056e43d
frednora <freddynora@gmail.com>
2023-04-15 18:16:05 -0300

    Gramado OS: Version 1.8 Release 318.

--- core/os/kernel/include/admin/product/version.h
+++ core/os/kernel/include/admin/product/version.h
@@ -9,17 +9,17 @@
 
 // Current build number.
 // Updated by the builder.
-#define CURRENT_BUILD_NUMBER    316
+#define __CURRENT_BUILD_NUMBER    318
 
 // Version
 #define VERSION_MAJOR    1
-#define VERSION_MINOR    7
-#define VERSION_BUILD    CURRENT_BUILD_NUMBER
+#define VERSION_MINOR    8
+#define VERSION_BUILD    __CURRENT_BUILD_NUMBER
 // Version word.
-#define VERSION_WORD    0x0107
+#define VERSION_WORD    0x0108
 // Strings
-#define VERSION_STRING    "1.7"
-#define BUILD_STRING      "316"
+#define VERSION_STRING    "1.8"
+#define BUILD_STRING      "318"
 #define RELEASE_STRING    PRODUCT_TYPE_STRING
 
 #endif
