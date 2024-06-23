7d91a0eb9f05710c75ff6a8500de621e315a075a
Fred Nora <freddynora@gmail.com>
2024-06-23 17:55:12 -0300

    netd: Registering the server.

--- userland/netd/server/connect.c
+++ userland/netd/server/connect.c
@@ -1,59 +1,53 @@
-
 // connect.c
-
+// Registering the gns server.
+// Created by Fred Nora.
 
 #include <gnsint.h>
 
 // Is it registered?
-static int __ns_registerd = -1;
-// Our desktop;
-// This structure is defined in the libgui.
-// Opaque pointer.
-struct desktop_d *__ns_desktop;
+static int __ns_registerd = FALSE;
+// Let's store a ring0 pointer right here. hahaha.
+static void *__ns_cgroup;
 // Our PID.
-pid_t __ns_pid=0;
+static pid_t __ns_pid=0;
 
 // ==========================================
 
+// Getting the current cgroup and 
+// register network server as the current server for this cgroup.
 // OUT:
 // 0   = Ok   
 // < 0 =  fail.
 int register_ns(void)
 {
-// Desktop:
-// Getting current desktop;
-// Register
-// Register network server as the current server for this desktop.
+    __ns_registerd = FALSE;
 
-    //__ns_registerd = FALSE;
-    __ns_registerd = -1;
-
-// Desktop:
-    __ns_desktop = 
-        (struct desktop_d *) gramado_system_call (519,0,0,0);
-
-    if ( (void *) __ns_desktop == NULL ){
-        debug_print ("register_ns: __ns_desktop fail\n");
-        return (int) (-1);
+// cgroup
+    __ns_cgroup = (void *) gramado_system_call(519,0,0,0);
+    if ((void *) __ns_cgroup == NULL){
+        debug_print ("register_ns: __ns_cgroup fail\n");
+        goto fail;
     }
 
 // PID
     __ns_pid = (int) getpid();
     if (__ns_pid < 0){
         debug_print ("register_ns: __ns_pid fail \n");
-        return (int) (-1);
+        goto fail;
     }
 
 // Register this pid as ns.
     gramado_system_call ( 
         (unsigned long) 521, 
-        (unsigned long) __ns_desktop, 
+        (unsigned long) __ns_cgroup, 
         (unsigned long) (__ns_pid & 0xFFFFFFFF), 
         (unsigned long) (__ns_pid & 0xFFFFFFFF) );
 
     __ns_registerd = TRUE;
 // O = OK.
     return 0;
+fail:
+    return (int) -1;
 }
 
 //
