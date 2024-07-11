4128fd3342ce97f075b6e6ba55439ba54acf39fc
Fred Nora <freddynora@gmail.com>
2024-07-11 09:14:26 -0300

    userland: Introducing the toolkits in this project.

---
+++ userland/core/toolkits/tkuser/tkuser.c
@@ -0,0 +1,11 @@
+
+// The goal for this project is create a toolkit
+// with wrapper function for the libc. Making the task of
+// creating  applications  easier.
+
+//ex:
+int tkuser_initialize(void)
+{
+    return 0;
+}
+
